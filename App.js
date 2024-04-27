6c879b3551da0beb7144fdcbc6a38bde7c186ce1
MauricioPatino <mauri.patino@gmail.com>
2024-04-26 21:55:38 -0400

    First push

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.box}>Welcome to ChatApp!</Text>
+      <View style={styles.fixToText}>
+        <Button
+          title="Sign In"
+          onPress={() => Alert.alert('Left button pressed')}
+        />
+        <Button
+          title="Continue as Guest"
+          onPress={() => Alert.alert('Right button pressed')}
+        />
+        </View>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
@@ -17,4 +29,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 25,
+    padding: 10,
+    borderColor: "black",
+    borderWidth: 5,
+    borderRadius: 10,
+  },
+    fixToText: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '80%',
+    },
+
 });
